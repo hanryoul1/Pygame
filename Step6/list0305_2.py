26e96336823bc8409dc40a8bbecdfed0d7c030d1
hanryoul1 <1228gksfbf@naver.com>
2021-10-17 13:51:19 +0900

    함수

---
+++ Step6/list0305_2.py
@@ -0,0 +1,8 @@
+# 인수를 갖는 함수
+
+def recover(val):
+    print("당신의 체력은")
+    print(val)
+    print("회복했다")
+
+recover(100)
---
+++ Step6/list0305_3.py
@@ -0,0 +1,7 @@
+# 값을 반환하는 함수
+
+def add(a,b):
+    return a + b
+
+c = add(1,2)
+print(c)
