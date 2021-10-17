1e622f195a4d01d247680567f42ab307fc4e980b
hanryoul1 <1228gksfbf@naver.com>
2021-10-18 01:01:44 +0900

    캘린더 복습

---
+++ Step7/list0402_1.py
@@ -0,0 +1,4 @@
+# 모듈 기본 사용법
+
+import calendar                     # import {모듈명}
+print(calendar.month(2021,10))      # {모듈명}.{해당 모듈에서 사용할 함수}
---
+++ Step7/list0402_2.py
@@ -0,0 +1,4 @@
+# 윤년 확인하기
+
+import calendar                  # calendar 모듈 임포트
+print(calendar.isleap(2021))     # 2021년이 윤년인지 확인
