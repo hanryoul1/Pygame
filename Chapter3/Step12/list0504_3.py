b492be7747c79c08e247a7f0cb0978cbe9b42a77
hanryoul1 <1228gksfbf@naver.com>
2021-10-22 01:25:08 +0900

    단계2: 답변을 입력받아 판정하기

---
+++ Chapter3/Step12/list0504_3.py
@@ -0,0 +1,18 @@
+# 단계2: 답변을 입력받아 판정하기
+
+import random
+ALP = ["A", "B", "C", "D", "E", "F", "G"]
+r = random.choice(ALP)
+alp = ""
+
+for i in ALP:
+    if i != r:
+        alp = alp + i
+print(alp)
+
+ans = input("빠진 알파벳은?")
+if ans == r:
+    print("정답입니다")
+
+else:
+    print("틀렸습니다")
