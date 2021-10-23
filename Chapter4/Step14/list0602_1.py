0c4ab962b3e9ef5f0e7425f8efe507d94d0dc053
hanryoul1 <1228gksfbf@naver.com>
2021-10-24 02:21:13 +0900

    라벨 배치

---
+++ Chapter4/Step14/list0602_1.py
@@ -0,0 +1,14 @@
+# 라벨 배치
+
+import tkinter
+root = tkinter.Tk()
+root.title("첫 번째 라벨")
+root.geometry("800x600")
+
+label = tkinter.Label(root, text = "라벨 문자열", font = ("System", 24)) 
+# 라벨 변수명 = tkinter.Label(윈도우 객체, text = "라벨 문자열", font = ("폰트 명", 폰트 크기))
+
+label.place(x=300, y=100)
+# {라벨 변수명}.place(x=X좌표, y=Y좌표)
+
+root.mainloop()
