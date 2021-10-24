de5f70bf3bcda49b4c25138fb24806a17eebcec4
hanryoul1 <1228gksfbf@naver.com>
2021-10-25 00:40:26 +0900

    버튼 클릭 시 반응

---
+++ Chapter4/Step15/list0603_2.py
@@ -0,0 +1,13 @@
+# 버튼 클릭 시 반응
+
+import tkinter
+
+def click_btn():
+    button["text"] = "클릭했습니다"
+
+root = tkinter.Tk()
+root.title("첫 번째 버튼")
+root.geometry("800x600")
+button = tkinter.Button(root, text = "클릭하십시오", font=("한컴산뜻돋움", 24), command = click_btn)
+button.place(x=300, y=100)
+root.mainloop()
