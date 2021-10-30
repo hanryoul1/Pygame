65610afc9f1e0715014f8b042f770c9c1c8fa5fc
hanryoul1 <1228gksfbf@naver.com>
2021-10-30 17:00:50 +0900

    체크 여부 확인하기

---
+++ Chapter5/Step20/list0703_3.py
@@ -0,0 +1,24 @@
+# 체크 여부 확인하기
+
+import tkinter
+
+def check():
+    if cval.get() == True:
+        print("체크되어 있습니다")
+
+    else:
+        print("체크되어 있지 않습니다")
+
+root = tkinter.Tk()
+root.title("처음부터 체크된 상태 만들기")
+root.geometry("400x200")
+
+cval = tkinter.BooleanVar()
+# BlooleanVar() 객체 준비
+
+cval.set(False)
+# 객체 True 설정
+
+cbtn = tkinter.Checkbutton(text = "체크 버튼", variable = cval, command = check)
+cbtn.pack()
+root.mainloop()
