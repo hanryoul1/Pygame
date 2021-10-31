69e2283cdc7f83fa812207ca7208c2478cc1e04e
hanryoul1 <1228gksfbf@naver.com>
2021-10-31 21:44:33 +0900

    메세지 박스 사용 방법

--- Chapter5/Step21/list0704_1.py
+++ Chapter5/Step21/list0704_1.py
@@ -5,6 +5,13 @@ import tkinter.messagebox
 
 def click_btn():
     tkinter.messagebox.showinfo("Information", "You click the button")
+
+    # 메세지 박스 표시 명령어
+    # showinfo: 정보를 표시하는 메세지 박스
+    # showwarning: 경고를 표시하는 메세지 박스
+    # showerror: 에러를 표시하는 메세지 박스
+    # askyesno: '네', '아니오' 버튼이 있는 메세지 박스
+    # askokcancel: 'OK', '취소' 버튼이 있는 메세지 박스
     
 root = tkinter.Tk()
 root.title("First message box")
