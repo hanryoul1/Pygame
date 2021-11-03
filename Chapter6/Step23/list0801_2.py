b6ca4588deb3327eb24b1f962c7f727bf06c5b05
hanryoul1 <1228gksfbf@naver.com>
2021-11-04 01:26:57 +0900

    after() 명령 사용하기

---
+++ Chapter6/Step23/list0801_2.py
@@ -0,0 +1,18 @@
+# after() 명령 사용하기
+
+import tkinter
+tmr = 0 
+#type: int 
+#전역 변수
+
+def count_up():
+    global tmr  # 함수 안에서 전역 변수 값을 변경할 때 global 선언
+    tmr = tmr + 1
+    label["text"] = tmr
+    root.after(1000, count_up)
+
+root = tkinter.Tk()
+label = tkinter.Label(font=("한컴산뜻돋움", 80))
+label.pack()
+root.after(1000, count_up)
+root.mainloop()
