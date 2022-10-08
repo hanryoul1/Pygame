bd9b67afb513120193f72ec9d5c2a1020dd2a6d7
hanryoul1 <1228gksfbf@khu.ac.kr>
2022-10-08 18:30:16 +0900

    똥 피하기 게임

--- Basic/quiz.py
+++ Basic/quiz.py
@@ -34,21 +34,85 @@ clock = pygame.time.Clock()
 ######################################################################################################
 
 # 1. 사용자 게임 초기화 (배경화면, 게임 이미지, 좌표, 폰트 등)
-running = True # 게임이 진행중인가?
+# 배경 만들기
+background = pygame.image.load('C:\\Users\\한률\\Desktop\\VSC\Pygame\\Basic\\image\\background.png')
+
+# 캐릭터 만들기
+character = pygame.image.load('C:\\Users\\한률\\Desktop\\VSC\Pygame\\Basic\\image\\character.png')
+character_size = character.get_rect().size # 이미지의 크기를 구해옴
+character_width = character_size[0] # 캐릭터의 가로 크기
+character_height = character_size[1] # 캐릭터의 세로 크기
+character_x_pos = (screen_width / 2)  - (character_width / 2) 
+character_y_pos = screen_height - character_height 
+
+# 이동 위치
+to_x = 0
+charcater_speed = 10
+
+# 적 만들기
+enemy = pygame.image.load('C:\\Users\\한률\\Desktop\\VSC\Pygame\\Basic\\image\\enemy.png')
+enemy_size = enemy.get_rect().size # 이미지의 크기를 구해옴
+enemy_width = enemy_size[0] # 캐릭터의 가로 크기
+enemy_height = enemy_size[1] # 캐릭터의 세로 크기
+enemy_x_pos = random.randint(0, (screen_width - enemy_width))
+enemy_y_pos = 0 
+enemy_speed = 10
+ 
+# 폰트 정의
+game_font = pygame.font.Font(None, 40) # 폰트 객체 생성 (폰트, 크기)
+running = True
 while running:
-    dt = clock.tick(30) # 게임 화면의 초당 프레임 수 설정
+    dt = clock.tick(30)
 
     # 2. 이벤트 처리 (키보드, 마우스 등)
     for event in pygame.event.get(): 
         if event.type == pygame.QUIT: 
             running = False 
     
+        if event.type == pygame.KEYDOWN: # 키가 눌러졌는지 확인
+            if event.key == pygame.K_LEFT: # 캐릭터 왼쪽으로
+                to_x -= charcater_speed
+            elif event.key == pygame.K_RIGHT: # 캐릭터 오른쪽으로
+                to_x += charcater_speed
+
+        if event.type == pygame.KEYUP: # 방향키를 떼면 멈춤
+            if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
+                to_x = 0
+    
+
     # 3. 게임 캐릭터 위치 정의
+    character_x_pos += to_x
+
+    if character_x_pos < 0:
+        character_x_pos = 0
+    elif character_x_pos > screen_width - character_width:
+        character_x_pos = screen_width - character_width
+
+    enemy_y_pos += enemy_speed
+    if enemy_y_pos > screen_height:
+        enemy_y_pos = 0 
+        enemy_x_pos = random.randint(0, (screen_width - enemy_width))
 
     # 4. 충돌 처리
+    # 충돌 처리를 위한 rect 정보 업데이트
+    character_rect = character.get_rect()
+    character_rect.left = character_x_pos
+    character_rect.top = character_y_pos
+
+    enemy_rect = enemy.get_rect()
+    enemy_rect.left = enemy_x_pos
+    enemy_rect.top = enemy_y_pos
+
+    # 충돌 체크
+    if character_rect.colliderect(enemy_rect):
+        print("충돌했어요")
+        running = False
 
     # 5. 화면에 그리기
-
+    screen.blit(background, (0, 0)) # 배경 그리기 / (0, 0) 기준으로 오른쪽 아래로 그려짐
+    screen.blit(character, (character_x_pos, character_y_pos)) # 캐릭터 그리기
+    screen.blit(enemy, (enemy_x_pos, enemy_y_pos)) # 적 그리기
     pygame.display.update() 
 
+pygame.time.dely(2000)
 pygame.quit()
